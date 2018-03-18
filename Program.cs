3832f6fc9943e5e5b8e0d95492f8362b3c087f7d
krzysztofprz <34395591+krzysztofprz@users.noreply.github.com>
2018-03-18 16:23:47 +0100

    prime numbers palindrome
    The algorithm which finds prime numbers in a defined scope then multiplied individual prime number by itself and finally finds the biggest number in multiplied prime numbers which is a palindrome.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        public static int MAX = 100000;
+
+        static void Main(string[] args)
+        {
+            List<int> primes = getDoubledPrimes(MAX);
+            List<int> palindromesList = new List<int>();
+            foreach (int i in primes)
+            {
+                bool palindrome = IsPalindrome(i);
+                if (palindrome)
+                {
+                    palindromesList.Add(i);
+                }
+            }
+
+            foreach(int p in palindromesList)
+            {
+                int maxValue = palindromesList.Max();
+                Console.WriteLine("This is the biggest palindrome: " + maxValue);
+                break;
+            }
+            Console.ReadKey();
+        }
+
+        static List<int> getDoubledPrimes(int max)
+        {
+            List<int> primesList = new List<int>();
+            bool isPrime = true;
+            for (int i = 2; i < max; i++)
+            {
+                for (int j = 2; j < i / 2; j++)
+                {
+
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    primesList.Add(i * i);
+                }
+                isPrime = true;
+            }
+            return primesList;
+
+        }
+
+        public static bool IsPalindrome(int num)
+        {
+            int n = num;
+            int rev = 0;
+            while (n > 0)
+            {
+                int dig = n % 10;
+                rev = rev * 10 + dig;
+                n = n / 10;
+            }
+            return num == rev;
+        }        
+    }
+}
+
+
