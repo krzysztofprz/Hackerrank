179799468408f159b480669e49d509e9427140b0
Krzysztof <34395591+krzysztofprz@users.noreply.github.com>
2018-04-02 20:23:58 +0200

    aVeryBigSum
    This program generates an array defined by user and sums all values in the array.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace longArraySum
+{
+    class Program
+    {
+
+        static long aVeryBigSum(int n, long[] ar)
+        {
+
+            long[] firstArray = ar;
+            firstArray = new long[n];
+            long sum = ar.Sum();
+            Console.WriteLine(sum);
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp));
+            long result = aVeryBigSum(n, ar);
+
+            Console.ReadKey();
+        }
+    }
+}
