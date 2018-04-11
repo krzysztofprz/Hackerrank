11f33e897e6efd804775b01b27315d83ad980c18
krzysztofprz <34395591+krzysztofprz@users.noreply.github.com>
2018-04-11 21:39:42 +0200

    Gives absolute value of diagonal difference
    The program sums diagonal values from left to right and from right to left. Then subtracts sums in absolute value.

---
+++ DiagonalDifference.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static int DiagonalDifference(int[][] a)
+        {            
+            int[] firstArray = new int[a.Length];
+            int[] secondArray = new int[a.Length];            
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                firstArray[i] = a[i][i];
+                //Console.WriteLine("liczby pierwsze: " + a[i][i]);
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                secondArray[i] = a[i][a.Length - i - 1];
+                //Console.WriteLine("liczby drugie: " + a[i][a.Length-i-1]);
+            }
+
+            int firstArrayResult = firstArray.Sum();
+            int secondArrayResult = secondArray.Sum();
+            //Console.WriteLine("wynik pierwszy: " + firstArrayResult);
+            //Console.WriteLine("wynik drugi: " + secondArrayResult);
+            int result = Math.Abs(firstArrayResult - secondArrayResult);
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+           // TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[][] a = new int[n][];
+
+            for (int aRowItr = 0; aRowItr < n; aRowItr++)
+            {
+                a[aRowItr] = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
+            }
+
+            int DiagonalDifferenceResult = DiagonalDifference(a);
+            //Console.WriteLine(DiagonalDifferenceResult);
+            //Console.ReadKey();
+            
+            //tw.WriteLine(result);
+            
+            //tw.Flush();
+            //tw.Close();
+        }
+    }
+}
+
