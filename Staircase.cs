13a7561ca31c00816b1d702518d1cb7201f36da4
krzysztofprz <34395591+krzysztofprz@users.noreply.github.com>
2018-04-25 19:28:12 +0200

    Staircase - hackerrank task no. 7
    Aligning strings to the left or right and build a n-size array

---
+++ Staircase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Staircase(n);
+
+        }
+
+        static void Staircase(int n)
+        {
+            string[] firstArray = new string[n];
+            string x = "#";
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (i == 0)
+                {
+                    firstArray[i] = x;
+                }
+                else
+                {
+                    firstArray[i] += x + firstArray[i - 1];
+                }
+            }
+
+            foreach (string item in firstArray)
+            {
+                Console.WriteLine(item.PadLeft(n));
+            }
+            
+            Console.ReadKey();
+        }
+
+    }
+}
