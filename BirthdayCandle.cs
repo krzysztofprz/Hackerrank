05b49c2acb7d4577e9c5dbbff9c2f99c7cc9c1a5
krzysztofprz <34395591+krzysztofprz@users.noreply.github.com>
2018-04-26 17:54:42 +0200

    Returns sum of highest element
    The programs checks which number is the highest one then counts how many such numbers is in a array and returns that quantity.

---
+++ BirthdayCandle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayCandle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            int result = BirthdayCakeCandles(n, ar);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static int BirthdayCakeCandles(int n, int[] ar)
+        {
+            int[] firstArray = ar;
+            int maxValue = firstArray.Max();
+
+            var maxQuery = (from val in firstArray
+                           where val == maxValue
+                           select val).Count();
+            return maxQuery;
+        }
+    }
+}
