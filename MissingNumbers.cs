f7c57bb261fafbcfa027ec3852b1491bef20c41b
krzysztofprz <34395591+krzysztofprz@users.noreply.github.com>
2018-07-04 20:49:36 +0200

    Missing numbers
    Hackerrank task. There are arrays with the same values. Few values from second array were removed. The program is finding these values.

---
+++ MissingNumbers.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace MissingNumbers
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = Convert.ToInt32(Console.ReadLine());
+//            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+//            int m = Convert.ToInt32(Console.ReadLine());
+//            int[] brr = Array.ConvertAll(Console.ReadLine().Split(' '), brrTemp => Convert.ToInt32(brrTemp));
+
+//            Stopwatch stopwatch = new Stopwatch();
+//            stopwatch.Start();
+
+//            int[] result = missingNumbers(arr, brr);
+
+//            foreach(var item in result)
+//            {
+//                Console.WriteLine("Diff: " + item);
+//            }
+
+//            stopwatch.Stop();
+//            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+//            Console.ReadKey();
+//        }
+
+//        static int[] missingNumbers(int[] arr, int[] brr)
+//        {
+//            List<int> resultList = new List<int>();
+
+//            int dlugosc = brr.Length;
+//            if(brr.Max() > brr.Length)
+//            {
+//                dlugosc = brr.Max();
+//            }
+
+//            Console.WriteLine("Lenght: " + dlugosc);
+            
+//            for (int i = 0; i < dlugosc+1; i++)
+//            {
+//                if (arr.Count(x => x == i) != brr.Count(x => x == i))
+//                {
+//                    resultList.Add(i);
+//                }
+//            }
+//            return resultList.ToArray();
+//        }
+//    }
+//}
