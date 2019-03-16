f6134af67ee92105733f52b552ab2b2bde6d18dc
Krzysztof <34395591+krzysztofprz@users.noreply.github.com>
2019-03-16 10:23:03 +0100

    Find the smallest, positive integer in an array.
    Codility introduction task

---
+++ ArraysSmallestPossibleInteger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysSmallestPossibleInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { };
+            int res = Solution(arr);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+
+        public static int Solution(int[] A)
+        {
+            int result = 0;
+            Array.Sort(A);
+            int[] noDuplicates = A.Distinct().ToArray();
+            noDuplicates = noDuplicates.Where(x => x != 0).ToArray();
+
+            if (noDuplicates.Count() == 0 || noDuplicates.Max() < 1)
+                return result = 1;
+            else
+            {
+                noDuplicates = noDuplicates.Where(x => x > 0).ToArray();
+
+                for (int i = 0; i < noDuplicates.Count(); i++)
+                {
+                    if (i + 1 != noDuplicates[i])
+                    {
+                        result = i + 1;
+                        break;
+                    }
+                    else
+                        result = noDuplicates.Max() + 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ArraysSmallestPossibleIntegerUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysSmallestPossibleInteger
+{
+    [TestFixture]
+    public class UnitTests
+    {      
+        [Test]
+        public void SolutionMethodTest()
+        {
+            //Arrange
+            int expectedEmpty = 1;
+            int expectedZeros = 1;
+            int onlyMinuses = 1;
+            int validInput = 2;
+            int failedInput = 6;
+
+            int[] empty = { };
+            int[] zeros = { 0, 0, 0,0 };
+            int[] minuses = { -1000000, -9, -8, -3 };
+            int[] valid = {-2, 1, 3, 4, 6, 81, 1};
+
+
+            //Act
+            int resultEmpty = Program.Solution(empty);
+            int resultZeros = Program.Solution(zeros);
+            int resultMinuses = Program.Solution(minuses);
+            int resultValid = Program.Solution(valid);
+
+            //Assert
+            Assert.AreEqual(expectedEmpty, resultEmpty);
+            Assert.AreEqual(expectedZeros, resultZeros);
+            Assert.AreEqual(onlyMinuses, resultMinuses);
+            Assert.AreEqual(validInput, resultValid);
+            Assert.AreNotEqual(failedInput, resultValid);
+        }
+    }
+}
